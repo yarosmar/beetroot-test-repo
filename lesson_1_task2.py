65e54793bd264b3cad5f42295efb5f3cb794bc2c
yarosmar <63430213+yarosmar@users.noreply.github.com>
2020-04-17 15:41:42 +0100

    lesson

---
+++ lesson_1_task2.py
@@ -0,0 +1,13 @@
+print('#', '#', sep='        ')
+print('#', '#', sep='        ')
+print('##########')
+print('#', '#', sep='        ')
+print('#', '#', sep='        ')
+
+print('\n', '\n')
+
+print('##########')
+print('#', '#', sep='        ')
+print('#', '#', sep='        ')
+print('#', '#', sep='        ')
+print('##########')
