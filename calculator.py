103640510cb286d51f072c41261ea470861a9cc3
yarosmar <yarosmar@gmail.com>
2020-04-27 15:43:53 +0100

    calculator

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def calculate():
+    
+    operation = input()
+
+    number_1 = int(input())
+    number_2 = int(input())
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please try the program again.')
